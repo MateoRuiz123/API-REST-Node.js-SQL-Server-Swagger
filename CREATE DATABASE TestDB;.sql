679dd14cc6e4012ada6f1ed5e952a35c7802ce6f
Mateo R <mrivera@coordinadora.com>
2024-07-08 17:55:54 -0500

    chore: Refactor SQL script for creating TestDB and Inventory table

--- CREATE DATABASE TestDB;.sql
+++ CREATE DATABASE TestDB;.sql
@@ -1,15 +1,29 @@
 CREATE DATABASE TestDB;
-SELECT Name from sys.databases;
+IF NOT EXISTS (SELECT *
+FROM sys.databases
+WHERE name = 'TestDB')
+BEGIN
+    CREATE DATABASE TestDB;
+END
 
 USE TestDB;
-CREATE TABLE dbo.Inventory (
+CREATE TABLE dbo.Inventory
+(
     id INT,
     name NVARCHAR(50),
     quantity INT,
     PRIMARY KEY (id)
 );
-INSERT INTO dbo.Inventory VALUES (1, 'banana', 150);
-INSERT INTO dbo.Inventory VALUES (2, 'orange', 154);
+INSERT INTO dbo.Inventory
+VALUES
+    (1, 'banana', 150);
+INSERT INTO dbo.Inventory
+VALUES
+    (2, 'orange', 154);
 
-SELECT * FROM dbo.Inventory
+SELECT *
+FROM dbo.Inventory
 WHERE quantity > 152;
+
+SELECT Name
+from sys.databases;
